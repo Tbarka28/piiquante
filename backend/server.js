2813ca2205771deaf5f28773f99f8722667f06c5
taous <barkataous@yahoo.fr>
2023-02-03 21:07:10 +0100

    mettre en place serveur js

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+// importer le package http
+const http = require("http");
+// importer l'application
+const app = require("./app");
+// Création de la fonction pour le renvoi d'un port valide
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+// création de la fonction pour la gestion des erreurs
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+// créer le serveur
+const server = http.createServer(app);
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind);
+});
+
+server.listen(port);
