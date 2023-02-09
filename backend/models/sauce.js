5915cbcfe713d6f270321b3cabb4a02231d4e195
taous <barkataous@yahoo.fr>
2023-02-09 19:57:54 +0100

    création du fichier sauce.js de models

---
+++ backend/models/sauce.js
@@ -0,0 +1,18 @@
+//importer mongoose
+const mongoose = require("mongoose");
+// Création du model sauce
+const sauceSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  usersLiked: { type: [String] },
+  usersDisliked: { type: [String] },
+});
+
+module.exports = mongoose.model("sauce", sauceSchema);
